cf7f67dc1ea8bc62741a11f70e57e9e97da4bed9
Rick Alves <rick_07@hotmail.com.br>
2021-10-31 04:20:15 -0300

    add default envs

--- src/main/env/index.ts
+++ src/main/env/index.ts
@@ -4,10 +4,10 @@ import { EnvValidator } from './env-validator';
 dotenv.config();
 
 export const env = new EnvValidator({
-	serverPort: (process.env.SERVER_PORT && parseInt(process.env.SERVER_PORT, 10)),
+	serverPort: (process.env.SERVER_PORT && parseInt(process.env.SERVER_PORT, 10) || 8080),
 	serverHost: (process.env.SERVER_HOST || 'localhost'),
 	mysqlHost: (process.env.MYSQL_HOST || 'localhost'),
-	mysqlPort: (process.env.MYSQL_PORT && parseInt(process.env.MYSQL_PORT, 10)),
+	mysqlPort: (process.env.MYSQL_PORT && parseInt(process.env.MYSQL_PORT, 10) || 3306),
 	mysqlDatabase: (process.env.MYSQL_DATABASE || ''),
 	mysqlUser: (process.env.MYSQL_USER || ''),
 	mysqlPassword: (process.env.MYSQL_PASSWORD || ''),
