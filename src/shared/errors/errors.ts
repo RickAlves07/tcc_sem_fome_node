b29c25ef478ae883102eaad5243315e914322130
Rick Alves <rick_07@hotmail.com.br>
2021-11-11 12:52:51 -0300

    add errors

--- src/shared/errors/errors.ts
+++ src/shared/errors/errors.ts
@@ -1,18 +1,28 @@
 import Joi from 'joi';
 import { BadRequest, InternalServerError, NotFound, Unauthorized } from './http-status-errors';
 
+// Not Found
 export class PageNotFound extends NotFound {
 	constructor() {
 		super('Page not found.');
 	}
 }
 
+
+// Bad Request
 export class InvalidRequest extends BadRequest {
 	constructor(details: Joi.ValidationErrorItem[]) {
 		super('Invalid request data.', details);
 	}
 }
 
+export class EmailAlreadyRegistered extends BadRequest {
+	constructor() {
+		super('This email is already registered in another account');
+	}
+}
+
+// Internal Server Error
 export class UnableConnectDatabase extends InternalServerError {
 	constructor(details: Error) {
 		super('Unable to connect to the database.', details);
@@ -28,11 +38,5 @@ export class InternalDatabaseError extends InternalServerError {
 export class CannotStartApplication extends InternalServerError {
 	constructor(details: Error) {
 		super('Cannot start application.', details);
-	}
-}
-
-export class EmailAlreadyRegistered extends BadRequest {
-	constructor() {
-		super('This email is already registered in another account');
 	}
 }
