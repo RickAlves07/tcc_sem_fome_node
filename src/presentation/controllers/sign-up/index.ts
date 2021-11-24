ec9e3a8d541af878701cf039f30293e3db7d51e8
Rick Alves <rick_07@hotmail.com.br>
2021-11-24 01:42:20 -0300

    add sign-up route

--- src/presentation/controllers/sign-up/index.ts
+++ src/presentation/controllers/sign-up/index.ts
@@ -3,22 +3,23 @@ import { inject, injectable } from 'tsyringe';
 import { Controller, Post } from '@/presentation/decorators';
 import { BaseController, HttpRequest, HttpResponse } from '@/presentation/protocols';
 import { ok } from '@/shared/helper';
+import { ISignUp } from '@/domain/usecases';
 
 @injectable()
 @Controller('/sign-up')
 export class SignUpController extends BaseController {
 	constructor(
-		// @inject('SignUp')
-		// private SignUp: ISignUp,
+		@inject('SignUp')
+		private signUp: ISignUp,
 	){
 		super()
 	}
 
-	// @Post('/')
-	// async store(req: HttpRequest): Promise<HttpResponse> {
+	@Post('/')
+	async store(req: HttpRequest): Promise<HttpResponse> {
 
-	// 	const response = {};
+		const response = await this.signUp.new(req.body);
 
-	// 	return ok(response);
-	// }
+		return ok(response);
+	}
 }
