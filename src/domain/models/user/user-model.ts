6b317e0787ae6cab07bec5191bf01e915f2dce7d
Rick Alves <rick_07@hotmail.com.br>
2021-11-24 01:44:08 -0300

    fix wrong address colunm name

--- src/domain/models/user/user-model.ts
+++ src/domain/models/user/user-model.ts
@@ -12,13 +12,13 @@ export class UserModel extends Model {
 			activated_at: DataTypes.DATE,
 		}, {
 			sequelize: connection,
-			tableName: 'users'
+			tableName: 'users',
 		})
 		return UserModel;
 	}
 
 	static associate(models) {
-		UserModel.belongsTo(models.AddressModel, { foreignKey: 'id', as: 'user_address'});
+		UserModel.belongsTo(models.AddressModel, { foreignKey: 'address_id', as: 'user_address'});
 		UserModel.hasMany(models.ShipmentModel, { foreignKey: 'user_transporter_id', as: 'shipments_transporter_user'});
 		UserModel.hasMany(models.ShipmentModel, { foreignKey: 'user_distributor_id', as: 'shipments_distributor_user'});
 		UserModel.hasOne(models.OrganizationModel, { foreignKey: 'owner_id', as: 'user_organization'});
