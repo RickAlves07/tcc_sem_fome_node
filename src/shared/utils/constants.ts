5b61e044f05168080bd6705966222b27cceb36d7
Rick Alves <rick_07@hotmail.com.br>
2021-11-28 07:11:57 -0300

    add constants

--- src/shared/utils/constants.ts
+++ src/shared/utils/constants.ts
@@ -14,14 +14,40 @@ export const tempUserFiledData = 'waiting activation';
 
 export const donationPackagesStatus = {
 	WaitingATransporter: 'waiting a transporter',
-	WaitingForPickup:'waiting for pickup',
+	WaitingForPickup: 'waiting for pickup',
 	OnDeliveryRoute: 'on delivery route',
-	InTransport:'in transport',
 	Delivered: 'delivered',
 	Received: 'received',
 	ReturningToDonor: 'returning to donor',
 	Returned: 'returned',
 	Canceled: 'canceled',
+}
+
+export const donationsByStatusToReturnByRouteParam = {
+	'in-progress': [
+		donationPackagesStatus.WaitingATransporter,
+		donationPackagesStatus.WaitingForPickup,
+		donationPackagesStatus.OnDeliveryRoute,
+		donationPackagesStatus.ReturningToDonor,
+	],
+	'history': [
+		donationPackagesStatus.Delivered,
+		donationPackagesStatus.Received,
+		donationPackagesStatus.Returned,
+		donationPackagesStatus.Canceled,
+	],
+	'available': [
+		donationPackagesStatus.WaitingATransporter,
+	],
+	'on-the-way': [
+		donationPackagesStatus.WaitingATransporter,
+		donationPackagesStatus.WaitingForPickup,
+		donationPackagesStatus.OnDeliveryRoute,
+	],
+	'received': [
+		donationPackagesStatus.Delivered,
+		donationPackagesStatus.Received,
+	],
 }
 
 export const profilesTypes = {
