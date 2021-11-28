0ec9eee71c9eaa4fe4509a75fc547e0baf12c097
Rick Alves <rick_07@hotmail.com.br>
2021-11-28 08:22:50 -0300

    update shipment model

--- src/domain/models/shipment/shipment.ts
+++ src/domain/models/shipment/shipment.ts
@@ -1,9 +1,9 @@
 export type Shipment = {
 	id?: number;
 	user_transporter_id: number;
-	user_distributor_id: number;
-	collected_at: Date | null;
-	delivered_at: Date | null;
+	organization_distributor_id: number;
+	collected_at?: Date | null;
+	delivered_at?: Date | null;
 	created_at?: Date;
 	updated_at?: Date;
 };
