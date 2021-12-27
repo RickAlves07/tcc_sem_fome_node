806d53c7141db31ad05d6b57591da98f596cb25d
Rick Alves <rick_07@hotmail.com.br>
2021-12-26 23:58:07 -0300

    refactor list donations

--- src/data/usecases/donation-package/db-list-donations-packages.ts
+++ src/data/usecases/donation-package/db-list-donations-packages.ts
@@ -1,8 +1,10 @@
 import { injectable, inject } from 'tsyringe';
 import { ListDonationsPackages, IListDonationsPackages } from '@/domain/usecases';
-import { IDonationPackageRepository, IUserRepository } from '@/infra/db/repositories';
+import { IDonationPackageRepository, IOrganizationRepository, IUserRepository } from '@/infra/db/repositories';
 import { donationsByStatusToReturnByRouteParam, profilesTypes, statusToReturnByRouteParamList } from '@/shared/utils';
 import { User } from '@/domain/models/user';
+import { Organization } from '@/domain/models/organization';
+import Utilities from '@/shared/utils/utilities';
 
 @injectable()
 export class DbListDonationsPackages implements IListDonationsPackages {
@@ -13,31 +15,40 @@ export class DbListDonationsPackages implements IListDonationsPackages {
 
 		@inject('UserRepository')
 		private userRepository: IUserRepository,
+
+		@inject('OrganizationRepository')
+		private organizationRepository: IOrganizationRepository,
 	) {}
 
 	async list(params: ListDonationsPackages.Params): Promise<ListDonationsPackages.Result> {
 
+		const userData = await this.getUserData(params.auth_user.user_id);
+		const userOrganization = await this.getUserOrganizationByAddressId(userData.address_id);
+
 		let conditions: any = {
 			status: this.setListOfStatusToGetByProfile(params.route),
 		}
-
-		conditions = this.getConditionsByRoute(params, conditions);
-
 		let conditionsTransporter: any;
 
-		if(params.auth_user.profile_type === profilesTypes.Transporter &&
-			statusToReturnByRouteParamList.ShipmentInProgress){
-			conditionsTransporter = {
-				user_transporter_id: params.auth_user.user_id,
-			}
-		}
+		conditions = this.getConditionsByParameterRoute(params.route, conditions, userData, userOrganization);
 
-		const donationsPackages = await this.getDonations(params, conditions, conditionsTransporter);
+		conditionsTransporter = this.getConditionsTransporterByParameterRoute(params.route, conditionsTransporter, userData);
+
+		const donationsPackages = await this.getDonationsList(params, conditions, conditionsTransporter);
 
 		return donationsPackages;
 	}
 
-	private async getDonations(params:ListDonationsPackages.Params, conditions: any, conditionsTransporter?: any)
+	private async getUserData(userId: number) : Promise<User> {
+		return await this.userRepository.findById(userId);
+	}
+
+	private async getUserOrganizationByAddressId(addressId: number) : Promise<Organization>
+	{
+		return await await this.organizationRepository.findOrganizationByAddressId(addressId);
+	}
+
+	private async getDonationsList(params:ListDonationsPackages.Params, conditions: any, conditionsTransporter?: any)
 	{
 		return await this.donationPackageRepository.findByStatus(
 			params.pageIndex,
@@ -47,39 +58,48 @@ export class DbListDonationsPackages implements IListDonationsPackages {
 		);
 	}
 
-	private async getUserData(userId: number) : Promise<User> {
-		return await this.userRepository.findById(userId);
-	}
-
 	private setListOfStatusToGetByProfile(route: string) : string[] {
 		return donationsByStatusToReturnByRouteParam[route];
-
 	}
 
-	private getConditionsByRoute(params: ListDonationsPackages.Params, conditions: any){
-		switch(params.route){
+	private getConditionsByParameterRoute(route: string, conditions: any, userData: User, organizationData?: Organization) {
+		switch(route){
+			// Donor
 			case statusToReturnByRouteParamList.History:
 			case statusToReturnByRouteParamList.InProgress:
-				if(params.auth_user.profile_type === profilesTypes.Donor || params.auth_user.profile_type === profilesTypes.Distributor){
-					conditions = {
-						...conditions,
-						user_donor_id: params.auth_user.user_id,
-					}
+			case statusToReturnByRouteParamList.OnTheWay:
+				conditions = {
+					...conditions,
+					user_donor_id: userData.id,
 				}
 				break;
-			case statusToReturnByRouteParamList.onTheWay:
+			// Distributor
 			case statusToReturnByRouteParamList.Received:
-				if(params.auth_user.profile_type === profilesTypes.Distributor){
-					conditions = {
-						...conditions,
-						organization_distributor_id: params.auth_user.user_id,
-					}
+				conditions = {
+					...conditions,
+					organization_distributor_id: organizationData.id,
 				}
 				break;
-
 			default:
 				break;
 		}
 		return conditions;
+	}
+
+	private getConditionsTransporterByParameterRoute(route: string, conditionsTransporter: any, userData: User) {
+		if(userData.profile_type === profilesTypes.Transporter){
+			switch(route){
+				//Transporter
+				case statusToReturnByRouteParamList.ShipmentsHistory:
+				case statusToReturnByRouteParamList.ShipmentsInProgress:
+					conditionsTransporter = {
+						...conditionsTransporter,
+						user_transporter_id: userData.id,
+					}
+				default:
+					break;
+			}
+		}
+		return conditionsTransporter;
 	}
 }
